8cddefe01bb91675cbe99d39fdee26cb2e9df358
Hamidur Rahman <Hamidur0405@gmail.com>
2023-12-08 19:36:03 -0500

    added an entrypoint script for webserver and updated compose file

---
+++ scripts/airflow/webserver_entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+airflow db init;
+
+airflow connections add 'fs_local_conn' --conn-type 'fs' --conn-extra '{"path": "/"}';
+
+airflow connections add 'ddp_rest_api_conn' --conn-type 'http' --conn-host 'ddp-rest-api' --conn-port '7000';
+
+airflow users create --role ${AF_ROLE} --username ${AF_USERNAME} --password ${AF_PASS} \
+  --firstname ${AF_FIRSTNAME} --lastname ${AF_LASTNAME} --email ${AF_EMAIL};
+
+airflow webserver >> ${AIRFLOW_HOME}/logs/airflow-webserver.log 2>&1
+
