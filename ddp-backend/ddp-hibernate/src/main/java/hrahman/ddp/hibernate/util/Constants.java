37109910f4f0d6ee2345fcb9a95f9b38fd3d1358
Hamidur Rahman <Hamidur0405@gmail.com>
2023-12-22 15:49:28 -0500

    added ddp-vault as module and updated hibernate module to drop the db configuration from build time

--- ddp-backend/ddp-hibernate/src/main/java/hrahman/ddp/hibernate/util/Constants.java
+++ ddp-backend/ddp-hibernate/src/main/java/hrahman/ddp/hibernate/util/Constants.java
@@ -1,2 +1,10 @@
-package hrahman.ddp.hibernate.util;public class Constants {
+package hrahman.ddp.hibernate.util;
+
+public final class Constants {
+    public static final String DDP_DB_PATH = "secret/DDP-DB";
+
+    public static final String DDP_DB_DRIVER = "ddp_db_driver";
+    public static final String DDP_DB_URL = "ddp_db_url";
+    public static final String DDP_DB_USER = "ddp_db_user";
+    public static final String DDP_DB_PASS = "ddp_db_pass";
 }
